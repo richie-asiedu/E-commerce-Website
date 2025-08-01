29a6d48c3a7c96d118f826d59dbc301bd92f34ab
ASIEDU RICHMOND <richieasiedu19@gmail.com>
2025-08-01 11:44:09 +0000

    Fix GitHub Actions workflow for master branch and update deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/e-commerce-website/', // Add base path for GitHub Pages
+  base: '/E-commerce-Website/', // Add base path for GitHub Pages
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
