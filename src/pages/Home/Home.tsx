c111e16c53f0e7f2852a225eb86142b89632a79e
ASIEDU RICHMOND <richieasiedu19@gmail.com>
2025-08-01 11:09:26 +0000

    Fix critical Home component syntax error that was causing blank page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,19 +5,21 @@ import ProductSection from '@/components/Product/ProductSection'
 import DressStyleGrid from '@/components/Styles/DressStyleGrid'
 import Testimonials from '@/components/Testimonials/Testimonials'
 
+const Home = () => {
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, []);
 
-const Home = () => (
-   useEffect(() => {
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-    }, []),
-  <main>
-    <HeroSection />
-    <BrandBar />
-    <ProductSection title="NEW ARRIVALS" type="new" />
-    <ProductSection title="TOP SELLING" type="top" />
-    <DressStyleGrid />
-    <Testimonials />
-  </main>
-)
+  return (
+    <main>
+      <HeroSection />
+      <BrandBar />
+      <ProductSection title="NEW ARRIVALS" type="new" />
+      <ProductSection title="TOP SELLING" type="top" />
+      <DressStyleGrid />
+      <Testimonials />
+    </main>
+  );
+}
 
 export default Home
